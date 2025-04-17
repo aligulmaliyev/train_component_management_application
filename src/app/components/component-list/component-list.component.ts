7686c3d818640cf16be7484c5d8ef15add92979d
Nahid Jamalli <jamallinahid@gmail.com>
2025-04-18 02:49:48 +0400

    c

--- src/app/components/component-list/component-list.component.ts
+++ src/app/components/component-list/component-list.component.ts
@@ -13,11 +13,7 @@ import {
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
-import {
-  MatFormFieldControl,
-  MatFormFieldModule,
-} from '@angular/material/form-field';
-import { mockData } from '../../mock/data';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { TrainComponent } from '../../models/train-component.model';
 import { ComponentService } from '../../services/train-component.service';
 import { CommonModule } from '@angular/common';
@@ -54,42 +50,43 @@ export class ComponentListComponent implements AfterViewInit {
     'quantity',
     'actions',
   ];
-  components: MatTableDataSource<TrainComponent>;
+
+  components: MatTableDataSource<TrainComponent> = new MatTableDataSource();
 
   @ViewChild(MatPaginator) paginator: MatPaginator = new MatPaginator();
   @ViewChild(MatSort) sort: MatSort = new MatSort();
 
-  constructor(private componentService: ComponentService) {
-    this.components = new MatTableDataSource(mockData);
-  }
+  constructor(private componentService: ComponentService) { }
 
   ngAfterViewInit() {
     this.components.paginator = this.paginator;
     this.components.sort = this.sort;
+
+    this.getAllComponent();
   }
 
-  getAllComponent() {
-    this.componentService
-      .getAll()
-      .subscribe((data: any) => (this.components = data));
+  getAllComponent(page: number = 1, pageSize: number = 10) {
+    this.componentService.getAll('', page, pageSize).subscribe((data) => {
+      this.components.data = data;
+    });
   }
 
-  async searchComponent(event: Event) {
+  searchComponent(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    await this.componentService.search(filterValue);
-
-    this.setFirstPagePaginate();
+    this.componentService.search(filterValue).subscribe((data) => {
+      this.components.data = data;
+      this.setFirstPagePaginate();
+    });
   }
 
-  async onChangePaginate(e: PageEvent) {
-    await this.componentService
-      .getAll('', e.pageIndex, e.pageSize)
-      .subscribe((data: any) => (this.components = data));
-    this.setFirstPagePaginate();
+  onChangePaginate(e: PageEvent) {
+    this.getAllComponent(e.pageIndex + 1, e.pageSize);
   }
 
   saveAssignQuantity(id: number, quantity: number) {
-    this.componentService.assignQuantity(id, quantity);
+    this.componentService.assignQuantity(id, quantity).subscribe(() => {
+      this.getAllComponent();
+    });
   }
 
   setFirstPagePaginate() {
@@ -100,12 +97,12 @@ export class ComponentListComponent implements AfterViewInit {
 
   openDialog(): void {
     const dialogRef = this.dialog.open(ComponentCreateComponent, {
-      data: { name: 'Ali' },
+      data: { name: '' },
     });
 
     dialogRef.afterClosed().subscribe((result) => {
-      console.log('The dialog was closed');
-      console.log(result);
+      console.log('The dialog was closed', result);
+      this.getAllComponent();
     });
   }
 }
--- src/app/services/train-component.service.ts
+++ src/app/services/train-component.service.ts
@@ -5,7 +5,7 @@ import { TrainComponent } from '../models/train-component.model';
 
 @Injectable({ providedIn: 'root' })
 export class ComponentService {
-  private baseUrl = '/api/components';
+  private baseUrl = 'https://localhost:7258';
 
   constructor(private http: HttpClient) {}
 
@@ -18,20 +18,18 @@ export class ComponentService {
       .set('search', search)
       .set('page', page)
       .set('pageSize', pageSize);
-    return this.http.get<TrainComponent[]>(this.baseUrl, { params });
+    return this.http.get<TrainComponent[]>(`${this.baseUrl}/components`, { params });
   }
 
   create(component: Partial<TrainComponent>) {
-    return this.http.post<TrainComponent>(this.baseUrl, component);
+    return this.http.post<TrainComponent>(`${this.baseUrl}/components`, component);
   }
 
   assignQuantity(componentId: number, quantity: number) {
-    return this.http.post(`${this.baseUrl}/${componentId}/assign-quantity`, {
-      quantity,
-    });
+    return this.http.post(`${this.baseUrl}/components/${componentId}/assign-quantity/${quantity}`, {});
   }
 
   search(query: string): Observable<TrainComponent[]> {
-    return this.http.get<TrainComponent[]>(`${this.baseUrl}/search?q=${query}`);
+    return this.http.get<TrainComponent[]>(`${this.baseUrl}/components/?search=${query}`);
   }
 }
